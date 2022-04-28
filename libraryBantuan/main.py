e177d9d30b221fd687751aa63b44dec40f276f2e
Rama Bena <ramabena.rb@gmail.com>
2022-04-29 00:25:13 +0800

    buat decision tree dengan semua kata

--- libraryBantuan/main.py
+++ libraryBantuan/main.py
@@ -1,18 +1,34 @@
+from pprint import pprint
 import numpy as np
 import pandas as pd
 from gameKatla import GameKatla
+from tree import DecisionTree
 import os
 
-if __name__ == "__main__":
-    file_path = "D:\\Rama Nitip\\python\\katla\\file text\\lima huruf katla.csv"
-    daftar_kata = list(pd.read_csv(file_path, header=None)[0])
-    game = GameKatla(daftar_kata)
+# if __name__ == "__main__":
+#     file_path = "D:\\Rama Nitip\\python\\katla\\file text\\lima huruf katla.csv"
+#     daftar_kata = list(pd.read_csv(file_path, header=None)[0])
+#     game = GameKatla(daftar_kata)
 
-    respon = 'y'
-    while respon == 'y':
-        print("PERMAINAN KATLA")
-        game.mulai_game()
-        respon = input("Ingin bermain lagi?[y/n] : ")
-        if respon == 'y':
-            os.system('cls')
-    print(f"SKOR AKHIR ANDA : {game.skor}")
+#     respon = 'y'
+#     while respon == 'y':
+#         print("PERMAINAN KATLA")
+#         game.mulai_game()
+#         respon = input("Ingin bermain lagi?[y/n] : ")
+#         if respon == 'y':
+#             os.system('cls')
+#     print(f"SKOR AKHIR ANDA : {game.skor}")
+
+PATH_FILE_TEXT = "D:\\Rama Nitip\\python\\katla\\file text\\"
+daftar_kata = list(pd.read_csv(PATH_FILE_TEXT + 'lima huruf katla.csv', header=None)[0])
+print(f"Banyak Kata : {len(daftar_kata)}")
+
+with open(PATH_FILE_TEXT+"coba.csv") as file:
+    df_sample = file.readlines()
+df_sample = [kata[:-1] for kata in df_sample]
+print(df_sample)
+
+DT = DecisionTree()
+DT.buat_tree(df_sample, pakai_semua_daftar=True)
+
+# pprint(DT.buat_dictionary(DT.root))
--- libraryBantuan/tree.py
+++ libraryBantuan/tree.py
@@ -12,8 +12,8 @@ from pprint import pprint
 
 class Node():
     def __init__(self, kata, daftar_kata):
-        self.kata = kata
-        self.daftar_kata = daftar_kata 
+        self.tebakan = kata
+        self.kandidat_jawaban = daftar_kata 
         self.next_node = {}
 
 class DecisionTree():
@@ -26,14 +26,16 @@ class DecisionTree():
             self.root = self.jadikan_object(object_dict)
         self.now = self.root
         self.pakai_semua_daftar = False
-        self.daftar_kata_semua = None  
 
-    def kata_berikutnya(self, pola, awal=False):
+    def kata_berikutnya(self, pola, tampilkan_kandidat=False, awal=False):
         if awal:
             self.now = self.root
         if pola in self.now.next_node:
             self.now = self.now.next_node[pola]
-        return self.now.kata
+            
+            if tampilkan_kandidat:
+                print(self.now.kandidat_jawaban)
+        return self.now.tebakan
 
     def buat_tree(self, daftar_kata, kata_root=None, pakai_semua_daftar=False):
         """membuat tree
@@ -44,40 +46,37 @@ class DecisionTree():
             pakai_semua_daftar (bool, optional): pakai daftar kata semua untuk split. Defaults to False.
         """        
         
-
-        # Bagian yang pertama kali jalan
         self.pakai_semua_daftar = pakai_semua_daftar
-        self.daftar_kata_semua  = daftar_kata
         if kata_root == None:
-            kata_root = self.cari_kata_terbaik(daftar_kata)
-        self.root = self.__buat_tree(kata_root, daftar_kata, 0)
+            kata_root = self.cari_kata_terbaik(daftar_kata, daftar_kata)[0]
+        self.root = self.__buat_tree(kata_root, daftar_kata, daftar_kata, 0)
         self.now  = self.root
     
-    def __buat_tree(self, kata, daftar_kata, depth): # buat tree rekursif
-        node = Node(kata, daftar_kata)
-        if (depth==10) or (len(daftar_kata)<=1): # BASE CASE
+    def __buat_tree(self, kata, daftar_kata_split, kandidat_jawaban, depth): # buat tree rekursif
+        node = Node(kata, kandidat_jawaban)
+        if (depth==8) or (len(kandidat_jawaban)<=1): # BASE CASE
             return node
         
-        for idx, pola in enumerate(self.semua_pola):
-            if kata == 'sarit':
-                print(f"{idx} dari {len(self.semua_pola)}")
-            next_daftar_kata = self.kandidat(kata, pola, daftar_kata)
-            if len(next_daftar_kata)==0:        # tidak ada daftar kata dengan pola ini
+        for pola in self.semua_pola:
+            next_kandidat_jawaban = self.kandidat(kata, pola, kandidat_jawaban)
+            if len(next_kandidat_jawaban)==0:        # tidak ada daftar kata dengan pola ini
                 continue
-            next_kata = self.cari_kata_terbaik(next_daftar_kata)
-            node.next_node[pola] = self.__buat_tree(next_kata, next_daftar_kata, depth+1)
+            cari_kata_terbaik = self.cari_kata_terbaik(daftar_kata_split, next_kandidat_jawaban)
+            next_kata = cari_kata_terbaik[0]
+            next_daftar_kata_split = cari_kata_terbaik[1]
+            node.next_node[pola] = self.__buat_tree(next_kata, next_daftar_kata_split, next_kandidat_jawaban, depth+1)
         return node
     
     def kandidat(self, tebakan, pola, daf_kata):
         pola_regex = [r"\w"]*5
         huruf_tanda_tanya = [tebakan[i] for i in range(5) if pola[i]=='?']
-        huruf_bintang = ''.join([tebakan[i] for i in range(5) if (pola[i]=='*') & (tebakan[i] not in huruf_tanda_tanya)])
+        huruf_bintang = ''.join([tebakan[i] for i in range(5) if (pola[i]=='*') and (tebakan[i] not in huruf_tanda_tanya)])
 
         # Membentuk pola regex
         for i, p in enumerate(pola):
             if p == '!':
                 pola_regex[i] = tebakan[i]
-            elif p == '?':
+            elif p == '?' or (p=='*' and tebakan[i] in huruf_tanda_tanya):
                 pola_regex[i] = f"[^ {tebakan[i]+huruf_bintang}]"
             elif p == '*':
                 pola_regex[i] = f"[^ {huruf_bintang}]"
@@ -86,12 +85,11 @@ class DecisionTree():
         daf_kata = re.findall(pola_regex, ' '.join(daf_kata))
 
         # Mengambil kata2 yang ada huruf ?
-        list_harus_ada_huruf = [tebakan[i] for i in range(5) if pola[i]=='?']
         new_daf_kata = []
         for kata in daf_kata:
             kata_tanpa_benar = [kata[i] for i in range(5) if pola[i]!='!']
             sudah_benar = True
-            for harus_ada_huruf in list_harus_ada_huruf:
+            for harus_ada_huruf in huruf_tanda_tanya:
                 if harus_ada_huruf in kata_tanpa_benar:
                     kata_tanpa_benar.remove(harus_ada_huruf)
                 else:
@@ -100,32 +98,67 @@ class DecisionTree():
                 new_daf_kata.append(kata)
         return new_daf_kata
 
-    def cari_kata_terbaik(self, daftar_kata):
+    def cari_kata_terbaik(self, daftar_kata_split, daftar_kata):
         def P(n):
             return n / len(daftar_kata)
         def impurity(n):
             return ((n-1) / n) if n!=0 else 0
 
+        if len(daftar_kata)==1:
+            return (daftar_kata[0], daftar_kata)
+
         hasil = ("TIDAK ADA", -10000)
+        new_daftar_kata_split = []
         impurity_awal = impurity(len(daftar_kata))
+        my_set = set(''.join(daftar_kata))
+        
+
         # Cari kata terbaik
-        loop_daftar_kata = self.daftar_kata_semua if self.pakai_semua_daftar else daftar_kata
-        for kata in loop_daftar_kata:
+        for kata in daftar_kata_split:
             impurity_akhir = 0
-            for pola in self.semua_pola:         # untuk setiap pola hitung kemungkinannya
-                n = len(self.kandidat(kata, pola, daftar_kata))
-                impurity_akhir += P(n) * impurity(n)
+            if len(my_set.intersection(set(kata)))==0: # kalau tidak ada satupun huruf yg sama dengn daftar kandidat
+                continue
             
+            daftar_pola = {}
+            for kandidat_jawaban in daftar_kata: # Cari segala kemungkinan pola
+                pola = self.getPola(kata, kandidat_jawaban)
+                daftar_pola[pola] = daftar_pola[pola]+1 if (pola in daftar_pola) else 1
+
+            for pola, n in daftar_pola.items(): # Hitung nilai impurity akhir
+                if pola == "!!!!!":
+                    impurity_akhir -= 0.1
+                else:
+                    impurity_akhir += P(n)*impurity(n)
+
             information_gain = impurity_awal - impurity_akhir           
-            if information_gain>hasil[1]:
+            if information_gain>hasil[1]:    # Ganti kata terbaik jika information gain lebih besar
                 hasil = (kata, information_gain)
-        return hasil[0]
+            new_daftar_kata_split.append(kata)
+        return hasil[0], new_daftar_kata_split
+
+    def getPola(self, tebakan, jawaban):
+        list_jawaban = []
+        respon_pola = ['*']*5
+        for i in range(5):
+            if jawaban[i] == tebakan[i]:
+                respon_pola[i] = '!'
+            else:
+                list_jawaban.append(jawaban[i])
+        
+        # yellow check / ada tapi salah tempat
+        for i in range(5):
+            if respon_pola[i] == '!':
+                continue
+            if tebakan[i] in list_jawaban:
+                respon_pola[i] = '?'
+                list_jawaban.remove(tebakan[i])
+        return ''.join(respon_pola)
 
     def buat_dictionary(self, now):
         hasil = {
-            'kata'       : now.kata,
-            'daftar kata': now.daftar_kata,
-            'next'       : {k:self.buat_dictionary(v) for k,v in now.next_node.items()}
+            'kata tebakan'     : now.tebakan,
+            'kandidat jawaban' : now.kandidat_jawaban,
+            'next pola'        : {k:self.buat_dictionary(v) for k,v in now.next_node.items()}
         }
 
         return hasil
@@ -141,12 +174,12 @@ class DecisionTree():
             return object_dict
 
     def jadikan_object(self, object_dict):
-        kata = object_dict['kata']
-        daftar_kata = object_dict['daftar kata']
+        tebakan = object_dict['kata tebakan']
+        kandidat_jawaban = object_dict['kandidat jawaban']
         next_node = {}
-        node = Node(kata, daftar_kata)
+        node = Node(tebakan, kandidat_jawaban)
 
-        for pola, next_dict in object_dict['next'].items():
+        for pola, next_dict in object_dict['next pola'].items():
             next_node[pola] = self.jadikan_object(next_dict)
         node.next_node = next_node
         return node
